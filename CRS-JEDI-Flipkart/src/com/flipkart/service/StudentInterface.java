960750470b93951f9030d201a790a50016cc6165
subha <subhanjan.saha@flipkart.com>
2021-05-24 19:16:37 +0530

    Student interface dummy code added

--- CRS-JEDI-Flipkart/src/com/flipkart/service/StudentInterface.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/service/StudentInterface.java
@@ -8,5 +8,19 @@ package com.flipkart.service;
  *
  */
 public interface StudentInterface {
+    public int register(String name,String userID,String password,Gender gender,int batch,String branch,String address,String country) throws StudentNotRegisteredException;
 
+    /**
+     * Method to get Student ID from User ID
+     * @param userId
+     * @return Student ID
+     */
+    public int getStudentId(String userId);
+
+    /**
+     * Method to check if student is approved by Admin or not
+     * @param studentId
+     * @return boolean indicating if student is approved
+     */
+    public boolean isApproved(int studentId);
 }
