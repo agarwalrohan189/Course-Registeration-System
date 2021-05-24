3b11a0f1ff9632e7decd9962a83bb2a87b7a8215
Sayantan Pal <sayantanpal2000.2018@gmail.com>
2021-05-24 22:42:50 +0530

    Added Admin and Login UI

---
+++ CRS-JEDI-Flipkart/src/com/flipkart/client/AdminMenu.java
@@ -0,0 +1,121 @@
+/**
+ * 
+ */
+package com.flipkart.client;
+
+import java.util.Scanner;
+
+/**
+ * @author sayan
+ *
+ */
+public class AdminMenu {
+	Scanner scanner = new Scanner(System.in);
+	public void printAdminMenu() {
+		boolean looping = true;
+		while(looping) {
+			System.out.println("_________________________________________");
+			System.out.println("Admin Menu");
+			System.out.println("1. View Course Catalog");
+			System.out.println("2. Add Course");
+			System.out.println("3. Delete Course");
+			System.out.println("4. View Pending Users' Sign Up Approval");
+			System.out.println("5. Approve Users' Sign Up");
+			System.out.println("6. Logout");
+			System.out.println("_________________________________________");
+			int optionChosen = scanner.nextInt();
+			
+			switch(optionChosen) {
+			case 1:
+				viewCourseCatalogue();
+				break;
+				
+			case 2:
+				addCourse();
+				break;
+				
+			case 3:
+				deleteCourse();
+				break;
+				
+			case 4:
+				viewPending();
+				break;
+			
+			case 5:
+				approveUsers();
+				break;
+			
+			case 6:
+				looping = false;
+				break;
+		}
+	}
+}
+	private void approveUsers() {
+		// TODO Auto-generated method stub
+//		System.out.println("called approveUsers()");
+		System.out.println("__________________________");
+		System.out.println("1. Approve All");
+		System.out.println("2. Approve All Students");
+		System.out.println("3. Approve All Professors");
+		System.out.println("4. Approve specific users");
+		System.out.println("__________________________");
+		int optionChosen = scanner.nextInt();
+		switch(optionChosen) {
+		case 1:
+			approveAll();
+			break;
+			
+		case 2:
+			approveAllStudents();
+			break;
+			
+		case 3:
+			approveAllProfessors();
+			break;
+			
+		case 4:
+			approveSpecficUsers();
+			break;
+		}
+	}
+		
+	private void approveSpecficUsers() {
+		// TODO Auto-generated method stub
+		System.out.println("Enter comma separated user ids: ");
+		String inputUIDs = scanner.next();
+		System.out.println("Approved users " + inputUIDs);
+	}
+	private void approveAllProfessors() {
+		// TODO Auto-generated method stub
+		System.out.println("Approved All New Professors");
+	}
+	private void approveAllStudents() {
+		// TODO Auto-generated method stub
+		System.out.println("Approved All New Students");
+	}
+	private void approveAll() {
+		// TODO Auto-generated method stub
+		System.out.println("Approved All New Users");
+	}
+	
+	
+	private void viewPending() {
+		// TODO Auto-generated method stub
+		System.out.println("called viewPending()");
+	}
+	
+	private void deleteCourse() {
+		// TODO Auto-generated method stub
+		System.out.println("called deleteCourse()");
+	}
+	private void addCourse() {
+		// TODO Auto-generated method stub
+		System.out.println("called addCourse()");
+	}
+	private void viewCourseCatalogue() {
+		// TODO Auto-generated method stub
+		System.out.println("called viewCourseCatalogue()");
+	}
+}
---
+++ CRS-JEDI-Flipkart/src/com/flipkart/client/LoginMenu.java
@@ -0,0 +1,86 @@
+/**
+ * 
+ */
+package com.flipkart.client;
+
+import java.util.Scanner;
+
+/**
+ * @author sayan
+ *
+ */
+public class LoginMenu {
+
+	/**
+	 * @param args
+	 */
+	private static Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		showMainMenu();
+	}
+
+	public static void showMainMenu() {
+		System.out.println("________________________");
+		System.out.println("Welcome");
+		System.out.println("1. Registration");
+		System.out.println("2. Login");
+		System.out.println("3. Exit");
+		System.out.println("________________________");
+		int optionChosen = scanner.nextInt();
+		switch(optionChosen) {
+		case 1:
+			registration();
+			break;
+			
+		case 2:
+			login();
+			break;
+			
+		case 3:
+			exit();
+			break;
+		}
+	}
+
+	private static void exit() {
+		// TODO Auto-generated method stub
+		System.out.println("Exit Application");
+	}
+
+	private static void login() {
+		// TODO Auto-generated method stub
+		System.out.println("Enter username: ");
+		String uName = scanner.next();
+		System.out.println("Enter password: ");
+		String password = scanner.next();
+		loginMain(uName, password);
+	}
+
+	private static void loginMain(String uName, String password) {
+		// TODO Auto-generated method stub
+		System.out.println("Login " + uName);
+	}
+
+	private static void registration() {
+		// TODO Auto-generated method stub
+		System.out.println("Enter name: ");
+		String name = scanner.next();
+		System.out.println("Enter gender: ");
+		String gender = scanner.next();
+		System.out.println("Enter dob: ");
+		String dob = scanner.next();
+		System.out.println("Enter role: ");
+		String role = scanner.next();
+		System.out.println("Enter password: ");
+		String password = scanner.next();
+		registrationMain(name, gender, dob, role, password);
+	}
+
+	private static void registrationMain(String name, String gender, String dob, String role, String password) {
+		// TODO Auto-generated method stub
+		System.out.println("Register " + name);
+	}
+	
+	
+}
