85994869cb2a6277557e29432fe4797f6afcf7ec
shubh-shah <shubhshah09@gmail.com>
2021-05-25 11:08:42 +0530

    Menu implementation

--- CRS-JEDI-Flipkart/src/com/flipkart/client/StudentMenu.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/client/StudentMenu.java
@@ -5,20 +5,25 @@ package com.flipkart.client;
 
 import java.util.Scanner;
 
+import com.flipkart.service.StudentInterface;
+import com.flipkart.service.StudentOperation;
+
 /**
  * @author shubh
  *
  */
-public class StudentUI {
+public class StudentMenu {
 	String studentID;
-	public StudentUI(String studentID) {
+	StudentInterface studentInterface = new StudentOperation();
+
+	public StudentMenu(String studentID) {
 		this.studentID = studentID;
 	}
+
 	public void displayUI() {
-		//Display the options available for the Student
-		Scanner sc=new Scanner(System.in);
-		while (true)
-		{
+		// Display the options available for the Student
+		Scanner sc = new Scanner(System.in);
+		while (true) {
 			System.out.println("========= Available Operations for Student ========");
 			System.out.println("1. View Course Catalogue");
 			System.out.println("2. View Grades");
@@ -32,74 +37,81 @@ public class StudentUI {
 			int input = sc.nextInt();
 
 			switch (input) {
-				case 1: 
-					viewCourseCatalogue();
-					break;
-						
-				case 2: 
-					viewGrades();
-					break;
+			case 1:
+				viewCourseCatalogue();
+				break;
 
-				case 3:
-					register();
-					break;
+			case 2:
+				viewGrades();
+				break;
 
-				case 4:
-					addCourse();
-					break;
+			case 3:
+				register();
+				break;
 
-				case 5:
-					dropCourse();
-					break;
+			case 4:
+				addCourse();
+				break;
 
-				case 6:
-					viewRegisteredCourses();
-					break;
-						
-				case 7:
-					payFee();
-					break;
-						
-				case 8: 
-					//Logout
-					sc.close();
-					System.out.println("==================== Logging Out ====================");
-					return;
-						
-				default:
-					System.err.println("No such operation exists, valid choices 1, 2, 3, 4, 5 ,6 ,7 ,8");
+			case 5:
+				dropCourse();
+				break;
+
+			case 6:
+				viewRegisteredCourses();
+				break;
+
+			case 7:
+				payFee();
+				break;
+
+			case 8:
+				// Logout
+				sc.close();
+				System.out.println("==================== Logging Out ====================");
+				return;
+
+			default:
+				System.err.println("No such operation exists, valid choices 1, 2, 3, 4, 5 ,6 ,7 ,8");
 			}
 		}
 	}
+
 	private void viewCourseCatalogue() {
 		
 	}
+
 	private void viewGrades() {
-		
+
 	}
+
 	private void register() {
-		
+
 	}
+
 	private void addCourse() {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter CourseID");
 		int cid = sc.nextInt();
-		sc.close();		
+		sc.close();
 	}
+
 	private void dropCourse() {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter CourseID");
 		int cid = sc.nextInt();
-		sc.close();		
+		sc.close();
 	}
+
 	private void viewRegisteredCourses() {
-		
+
 	}
+
 	private void payFee() {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Amount");
 		int amount = sc.nextInt();
 		sc.close();
-		
+
 	}
 }
