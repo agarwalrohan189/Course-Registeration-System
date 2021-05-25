d7aa416beb3c58e7808222074c91821a4322dc1d
Shubham1427 <shubhamsingh14272000@gmail.com>
2021-05-25 22:55:10 +0530

    Role and sql queries added

--- CRS-JEDI-Flipkart/src/com/flipkart/constant/SQLQueries.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/constant/SQLQueries.java
@@ -9,7 +9,14 @@ package com.flipkart.constant;
  */
 public class SQLQueries {
 	
+	//UserDao queries
+		public static final String GET_USER_DETAILS_QUERY = "select * from users where id = ?";
+		
+	//ProfessorDao queries
+		public static final String GET_PROFESSOR_DETAILS_QUERY = "select * from professors where id = ?";
+	
 	//AdminDao Queries
+		public static final String GET_ADMIN_DETAILS_QUERY = "select * from admins where id = ?";
 		public static final String DELETE_COURSE_QUERY = "delete from Course where courseCode = ?";
 		public static final String ADD_COURSE_QUERY = "insert into Course(courseCode, courseName, catalogId) values (?, ?, ?)";
 		public static final String VIEW_PENDING_ADMISSION_QUERY = "select userId, name, password, role, gender, address, country, studentId from student natural join user where isApproved = 0";
@@ -49,4 +56,5 @@ public class SQLQueries {
 		public static final String GET_ENROLLED_STUDENTS="select course.courseCode,course.courseName,registeredcourse.studentId from course inner join registeredcourse on course.courseCode = registeredcourse.courseCode where course.professorId = ? order by course.courseCode";
 		public static final String NUMBER_OF_REGISTERED_COURSES=" select studentId from registeredcourse where studentId = ? ";
 		public static final String IS_REGISTERED=" select courseCode from registeredcourse where courseCode=? and studentId=? ";
+		public static final String GET_STUDENT_DETAILS_QUERY = "select * from students where id = ?";
 }
