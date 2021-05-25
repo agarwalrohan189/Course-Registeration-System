787c1843c0d052103f8d1bbe5b63b938fd878b19
go2hel <61565872+go2hel@users.noreply.github.com>
2021-05-25 14:53:24 +0530

    Some Interface updated

--- CRS-JEDI-Flipkart/src/com/flipkart/service/ProfessorInterface.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/service/ProfessorInterface.java
@@ -3,6 +3,8 @@ package com.flipkart.service;
 import com.flipkart.bean.Course;
 import com.flipkart.bean.Student;
 import com.flipkart.exception.GradeNotAssignedException;
+import com.flipkart.exception.ProfNotFoundException;
+import com.flipkart.exception.UserNotFoundException;
 
 import java.util.List;
 
@@ -15,14 +17,14 @@ public interface ProfessorInterface {
      * Method to view course which professor is teaching
      * @return -> List of courses professor is teaching
      */
-    public List<Course> viewCourses(String profID);
+    public List<Course> viewCourses(String profID) throws ProfNotFoundException;
 
     /**
      * Return enrolled student list for given course.
      * @param courseID -> ID of course whose students are requested.
      * @return -> List of students enrolled in course.
      */
-    public List<Student> viewStudent(int courseID, String profID);
+    public List<Student> viewStudent(int courseID, String profID) throws ProfNotFoundException;
 
     /**
      * Method to assign grade to student.
@@ -30,6 +32,6 @@ public interface ProfessorInterface {
      * @param courseID -> ID of course in which grade is given.
      * @param grade -> Grade given to student.
      */
-    public void assignGrade(String profID, String studentID, int courseID, String grade) throws GradeNotAssignedException;
+    public void assignGrade(String profID, String studentID, int courseID, String grade) throws GradeNotAssignedException, ProfNotFoundException, UserNotFoundException;
 
 }
--- CRS-JEDI-Flipkart/src/com/flipkart/service/ProfessorOperation.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/service/ProfessorOperation.java
@@ -3,23 +3,25 @@ package com.flipkart.service;
 import com.flipkart.bean.Course;
 import com.flipkart.bean.Student;
 import com.flipkart.exception.GradeNotAssignedException;
+import com.flipkart.exception.ProfNotFoundException;
+import com.flipkart.exception.UserNotFoundException;
 
 import java.util.List;
 
 public class ProfessorOperation implements ProfessorInterface{
 
     @Override
-    public List<Course> viewCourses(String profID) {
+    public List<Course> viewCourses(String profID) throws ProfNotFoundException {
         return null;
     }
 
     @Override
-    public List<Student> viewStudent(int courseID, String profID) {
+    public List<Student> viewStudent(int courseID, String profID) throws ProfNotFoundException{
         return null;
     }
 
     @Override
-    public void assignGrade(String profID, String studentID, int courseID, String grade) throws GradeNotAssignedException {
+    public void assignGrade(String profID, String studentID, int courseID, String grade) throws GradeNotAssignedException, ProfNotFoundException, UserNotFoundException {
 
     }
 }
