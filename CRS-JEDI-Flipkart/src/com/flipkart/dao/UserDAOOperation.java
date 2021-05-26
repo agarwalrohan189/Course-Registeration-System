f1e10c6ed42778fba1b426cfd980fef6a3dcd0b1
Shubham1427 <shubhamsingh14272000@gmail.com>
2021-05-26 11:02:55 +0530

    Login working

--- CRS-JEDI-Flipkart/src/com/flipkart/dao/UserDAOOperation.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/dao/UserDAOOperation.java
@@ -52,7 +52,7 @@ public class UserDAOOperation implements UserDAOInterface{
 		try
 		{
 			User user = getDetails(userID);
-			if (password != user.getPassword())
+			if (!password.equals (user.getPassword()))
 				throw new PasswordMismatchException(userID);
 			else
 				return user.getRole();
